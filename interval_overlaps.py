18506db2ed64b771aa9ae9248241034f2ed730c7
Andre Almeida <goios@hotmail.com>
2025-03-26 08:00:12 +1100

    interval list naive

--- interval_overlaps.py
+++ interval_overlaps.py
@@ -1,5 +1,32 @@
 from itertools import chain
 
-def interval_overlap(*intervals):
+def interval_overlaps(*intervals):
+    """
+        Tests whether any of the interval in argument overlaps
+        Accepts any number of intervals defined as strat and endpoint
+
+        > interval_overlaps([1,2],[3,4])
+
+        rerturns False
+    """
     ij = sorted(intervals)
     return list(chain(*ij)) != sorted(chain(*ij))
+
+def interval_list_overlaps(il1, il2):
+    """
+        Given two lists of intervals, returns the indices
+        of the intervals that overlap
+
+        > interval_list_overlaps([(1,3),(4,6)],[(2,3.5),(7,8)])
+
+        returns [(1,1)]
+    """
+    index_list = []
+    for n1, i1 in enumerate(il1):
+        for n2, i2 in enumerate(il2):
+            if interval_overlaps(i1, i2):
+                index_list.append((n1,n2))
+
+    return index_list
+                
+    
--- test_interval_overlaps.py
+++ test_interval_overlaps.py
@@ -1,32 +1,50 @@
 import interval_overlaps as io
 import unittest
 
+
 class TestIntervalOverlap(unittest.TestCase):
     def test_edge_ovl_interval_pair(self):
         i1 = [1,3]
         i2 = [2,4]
-        self.assertTrue(io.interval_overlap(i1,i2))
+        self.assertTrue(io.interval_overlaps(i1,i2))
         
     def test_inner_interval_pair(self):
         i1 = [1,2]
         i2 = [0,4]
-        self.assertTrue(io.interval_overlap(i1,i2))
+        self.assertTrue(io.interval_overlaps(i1,i2))
 
     def test_outer_interval_pair(self):
         i2 = [1,2]
         i1 = [0,4]
-        self.assertTrue(io.interval_overlap(i1,i2))
+        self.assertTrue(io.interval_overlaps(i1,i2))
 
     def test_non_ovl_interval_pair_rev(self):
         i2 = [1,2]
         i1 = [3,4]
-        self.assertFalse(io.interval_overlap(i1,i2))
+        self.assertFalse(io.interval_overlaps(i1,i2))
 
     def test_non_ovl_interval_pair_fw(self):
         i1 = [1,2]
         i2 = [3,4]
-        self.assertFalse(io.interval_overlap(i1,i2))
+        self.assertFalse(io.interval_overlaps(i1,i2))
 
+    def test_three_interval(self):
+        i1 = [1,3]
+        i2 = [3,5]
+        i3 = [4,6]
+        self.assertTrue(io.interval_overlaps(i1,i2,i3))
+
+
+class TestIntervalListOverlap(unittest.TestCase):
+    def test_single_interval_overlap(self):
+        il1 = [(1,3),(4,6)]
+        il2 = [(-1,0),(2,3.5),(8,9)]
+        self.assertEqual(io.interval_list_overlaps(il1,il2),[(0,1)])
+
+    def test_single_interval_overlaps_two(self):
+        il1 = [(1,3),(4,6)]
+        il2 = [(-1,0),(3.5,4.5),(5,9)]
+        self.assertEqual(io.interval_list_overlaps(il1,il2),[(1,1),(1,2)])
 
 if __name__ == "__main__":
     unittest.main()
